d2d00cd7ce79a865055539bdda6c3b9cdd7a64ec
Raihan <mdr8077@gmail.com>
2025-08-04 22:30:23 +0600

    Header responsive issue fixed

--- script/header-script.js
+++ script/header-script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     const mobileMenu = document.querySelector(".mobile-menu");
     mobileMenu.style.display = 'flex';
+    const sizeControl = document.querySelector("header iframe");
+    sizeControl.style.height = '100%';
 }
 function closeMenu() {
     const mobileMenu = document.querySelector(".mobile-menu");
