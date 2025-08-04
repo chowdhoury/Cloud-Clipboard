d35264c737cf9b191226ae18c97ca8f717ec4dd0
Raihan <mdr8077@gmail.com>
2025-08-04 19:47:35 +0600

    header responsive done

---
+++ script/header-script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    mobileMenu.style.display = 'flex';
+}
+function closeMenu() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    mobileMenu.style.display = 'none';
+}
